d84ba13c5bdc4b713f56f5f38b6576f7aa12210f
Luis Osorio <64318203+luisosorio258@users.noreply.github.com>
2021-03-03 20:28:30 -0300

    Create numerosAlgunsCuidados.js

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)//Gera infinito
+console.log("10" / 2)// se for formato de numero faz a operacao normal
+console.log("Show" * 2)
+console.log(0.1 + 0.7)
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
