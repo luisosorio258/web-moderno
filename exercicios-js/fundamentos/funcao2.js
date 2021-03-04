52116926aa3d4164d7c3beaa8997d520e96cf16b
Luis Osorio <64318203+luisosorio258@users.noreply.github.com>
2021-03-04 19:37:54 -0300

    Create funcao2.js

---
+++ exercicios-js/fundamentos/funcao2.js
@@ -0,0 +1,18 @@
+//Armazenar uma funcao em uma variavel
+const imprimirSoma = function (a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+//Armazenar uma funcao arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+//retorno implicito
+
+const subtracao = (a, b) => a - b
+console.log(subtracao(5, 2))
