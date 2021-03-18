7c92e0ba4ecd796390be9f5472f90536028f99bc
Luis Osorio <64318203+luisosorio258@users.noreply.github.com>
2021-03-18 19:50:01 -0300

    Create oo.js

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,22 @@
+// codigo nao executavel
+
+//procedural
+processamento(v1, v2, v3)
+
+//OO
+objeto = {
+    v1,
+    v2,
+    v3,
+    processamento(){
+        //....
+    }
+}
+
+objeto.processamento() // foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. heranca (prototype)
+// 4. polimorfismo
