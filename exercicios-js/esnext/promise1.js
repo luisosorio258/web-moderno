bf6b01970e1e6f1463bf47e867498514f9cc6296
Luis Osorio <64318203+luisosorio258@users.noreply.github.com>
2021-04-07 20:24:51 -0300

    Create promise1.js

---
+++ exercicios-js/esnext/promise1.js
@@ -0,0 +1,15 @@
+
+
+const primeiroElemento = array => array[0]
+
+const primeiraLetra = string => string[0]
+
+const letraMinuscula = letra => letra.toLowerCase()
+
+new Promise(function(resolve){
+    resolve(['Ana', 'Bia', 'Carlos', 'Daniel'])
+})
+    .then(primeiroElemento)
+    .then(primeiraLetra)
+    .then(letraMinuscula)
+    .then(console.log)
